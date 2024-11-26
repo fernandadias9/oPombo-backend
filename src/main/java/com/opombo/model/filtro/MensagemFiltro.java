c1a42fe9db1980fb091a9f40b232efa2d1b89c63
Vitor Stosick <vitor.stosick@alunos.sc.senac.br>
2024-11-26 10:46:42 -0300

    feat: adicionando filtro de mensagens curtidas pelo usuario autenticado

--- src/main/java/com/opombo/model/filtro/MensagemFiltro.java
+++ src/main/java/com/opombo/model/filtro/MensagemFiltro.java
@@ -1,10 +1,8 @@
 package com.opombo.model.filtro;
 
 import com.opombo.model.entity.Mensagem;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import com.opombo.model.entity.Usuario;
+import jakarta.persistence.criteria.*;
 import lombok.Data;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -18,12 +16,14 @@ public class MensagemFiltro extends BaseFiltro implements Specification<Mensagem
 
     private String texto;
     private String nomePublicador;
+    private String idUsuario;
     private LocalDate dataInicial;
     private LocalDate dataFinal;
 
     public boolean temFiltro() {
-        return  (filtroValido(this.texto))
+        return (filtroValido(this.texto))
                 || (filtroValido(this.nomePublicador))
+                || (filtroValido(this.idUsuario))
                 || (dataInicial != null)
                 || (dataFinal != null);
     }
@@ -32,17 +32,20 @@ public class MensagemFiltro extends BaseFiltro implements Specification<Mensagem
     public Predicate toPredicate(Root<Mensagem> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
         List<Predicate> predicates = new ArrayList<>();
 
-        if(this.getTexto() != null && this.getTexto().trim().length() > 0) {
+        if (this.getTexto() != null && this.getTexto().trim().length() > 0) {
             predicates.add(cb.like(root.get("texto"), "%" + this.getTexto() + "%"));
         }
 
-        if(this.getNomePublicador() != null && this.getNomePublicador().trim().length() > 0) {
-            //Predicado --> operador (comparação), atributo/coluna, valor
-            //Forma 1: usando somente get
+        if (this.getNomePublicador() != null && this.getNomePublicador().trim().length() > 0) {
             predicates.add(cb.like(root.get("publicador").get("nome"), "%" + this.getNomePublicador() + "%"));
         }
 
         aplicarFiltroPeriodo(root, cb, predicates, this.getDataInicial(), this.getDataFinal(), "criadoEm");
+
+        if (this.getIdUsuario() != null && !this.getIdUsuario().trim().isEmpty()) {
+            Join<Mensagem, Usuario> usuariosQueCurtiramJoin = root.join("usuariosQueCurtiram");
+            predicates.add(cb.equal(usuariosQueCurtiramJoin.get("id"), this.getIdUsuario()));
+        }
 
         return cb.and(predicates.toArray(new Predicate[0]));
     }
